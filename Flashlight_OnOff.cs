4d0568b897a59f7bf818abb70b9c3cd3d86cd1f0
wesselvink <22092@ma-web.nl>
2018-05-11 17:09:54 +0200

    Scripts

---
+++ Flashlight_OnOff.cs
@@ -0,0 +1,43 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Flashlight_OnOff : VRTK_InteractableObject
+    {
+        private bool isActive = true;
+
+        private Renderer ren;
+        private AudioSource audioS;
+
+        [SerializeField] GameObject Flashlight;
+        [SerializeField] Texture active, notActive;
+        [SerializeField] AudioClip click;
+
+        public override void StartUsing(VRTK_InteractUse usingObject)
+        {
+            base.StartUsing(usingObject);
+            ToggleFlash();
+            ren = GetComponent<Renderer>();
+            audioS = GetComponent<AudioSource>();
+            audioS.clip = click;
+        }
+
+        private void ToggleFlash()
+        {
+            if (isActive == false)
+            {
+                Flashlight.SetActive(true);
+                isActive = true;
+                ren.material.mainTexture = active;
+                audioS.Play();
+            }
+            else if (isActive == true)
+            {
+                Flashlight.SetActive(false);
+                isActive = false;
+                ren.material.mainTexture = notActive;
+                audioS.Play();
+            }
+        }
+    }
+}
---
+++ RandomSound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSound : MonoBehaviour {
+	public AudioClip[] clips;
+	public float endTimer;
+	public float Timer;
+
+	void Start () {
+	Timer = 0f;
+	endTimer = Random.Range(30f, 60f);
+	}
+
+	void Update () {
+		Timer += Time.deltaTime;
+		if (Timer >= endTimer){
+			playSound ();
+			Timer = 0;
+		}
+	}
+	void playSound (){
+	int randomClip = Random.Range (0, clips.Length);
+	AudioSource source = gameObject.AddComponent<AudioSource> ();
+		source.clip = clips[randomClip];
+		source.Play();
+		endTimer = Random.Range (30f, 60f);
+		Destroy (source, clips[randomClip].length);
+	}
+}
+
+
